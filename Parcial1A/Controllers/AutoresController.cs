642f01a17c9f9ad9ee1131fa5ebd99a86a1abf97
LilianaAguilar14 <92348509+LilianaAguilar14@users.noreply.github.com>
2024-03-09 13:44:40 -0600

    Controladores

---
+++ Parcial1A/Controllers/AutoresController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parcial1A.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Parcial1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        // GET: api/<AutoresController>
+        private readonly AutoresdbContext _autoresContext;
+
+        public AutoresController(AutoresdbContext autoresContext)
+        {
+            _autoresContext = autoresContext;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Autore> autores = new List<Autore>(from e in _autoresContext.Autores select e).ToList();
+            if (autores.Count == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(autores);
+            }
+        }
+
+        // GET api/<AutoresController>/5
+        [HttpGet("{id}")]
+        public IActionResult Getbyid(int id)
+        {
+            List<Autore> autores = _autoresContext.Autores.Where(u => u.Id == id).ToList();
+
+            if (autores.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(autores);
+        }
+
+        // POST api/<AutoresController>
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Post([FromBody] Autore publicaciones)
+        {
+            try
+            {
+                _autoresContext.Add(publicaciones);
+                _autoresContext.SaveChanges();
+                return Ok(publicaciones);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<AutoresController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AutoresController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Parcial1A/Controllers/AutoresLibroController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parcial1A.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Parcial1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresLibroController : ControllerBase
+    {
+        private readonly AutoresdbContext _autoresdbContext;
+
+        public AutoresLibroController(AutoresdbContext autoresdbContext)
+        {
+            _autoresdbContext = autoresdbContext;
+        }
+        // GET: api/<AutoresLibroController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<AutorLibro> autorlibro = new List<AutorLibro>(from e in _autoresdbContext.AutorLibros select e).ToList();
+            if (autorlibro.Count == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(autorlibro);
+            }
+        }
+        // GET api/<AutoresLibroController>/5
+        [HttpGet("{id}")]
+        public IActionResult Getbyid(int id)
+        {
+            List<AutorLibro> publicaciones = _autoresdbContext.AutorLibros.Where(u => u.AutorId == id).ToList();
+
+            if (publicaciones.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(publicaciones);
+        }
+
+        // POST api/<AutoresLibroController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AutoresLibroController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AutoresLibroController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
