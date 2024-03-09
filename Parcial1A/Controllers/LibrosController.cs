bef958c92b631c0ae9d847678bc8d5d557ccce38
Jairo Erazo <132089241+JairoVErazo@users.noreply.github.com>
2024-03-09 14:36:58 -0600

    Busqueda de libros por autor

--- Parcial1A/Controllers/LibrosController.cs
+++ Parcial1A/Controllers/LibrosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parcial1A.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,6 +10,12 @@ namespace Parcial1A.Controllers
     [ApiController]
     public class LibrosController : ControllerBase
     {
+        private readonly AutoresdbContext autoresdbContext;
+
+        public LibrosController( AutoresdbContext autoresdbContext)
+        {
+            this.autoresdbContext = autoresdbContext;
+        }
         // GET: api/<LibrosController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -38,6 +46,20 @@ namespace Parcial1A.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        [Route("autor/{nombreDelAutor}")]
+        public ActionResult<Libro> BuscarLibrosPorAutor(string nombreDelAutor)
+        {
+            var librosPorAutor = (from autor in autoresdbContext.Autores
+                                  join autorLibro in autoresdbContext.AutorLibros on autor.Id equals autorLibro.AutorId
+                                  join libro in autoresdbContext.Libros on autorLibro.LibroId equals libro.Id
+                                  where autor.Nombre == nombreDelAutor
+                                  select libro)
+                                  .Distinct()
+                                  .ToList();
+
+            return Ok(librosPorAutor);
         }
     }
 }
